bf64dea86f967ed406538b014df399f70c81466f
steph31090 <slramos310@gmail.com>
2025-12-07 20:11:00 -0800

    pushing 12/7/25

---
+++ module-9/Select5.java
@@ -0,0 +1,52 @@
+/*
+ *
+ * Stephanie Ramos
+ * Module 9 Programming Assignment
+ * 
+ * Purpose: Updated the connection to select data from my database.
+ */
+
+import java.sql.*;
+
+public class Select5{
+
+  public static void main(String args[]){
+
+    try{
+
+      Connection con;
+
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      String url = "jdbc:mysql://localhost:3307/databasedb?";
+
+      con = DriverManager.getConnection(url + "user=student1&password=pass");
+
+      System.out.println("Connection established - now executing a select");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM address33");
+
+     System.out.println("Received Results:");
+
+     int i = rs.getMetaData().getColumnCount();
+
+     while(rs.next()){
+
+       for(int x = 1; x <= i; ++x){
+
+         System.out.println(rs.getString(x));
+       }
+
+       System.out.println("");
+     }
+
+     stmt.close();
+     con.close();
+    }
+    catch (java.lang.Exception ex){
+
+      ex.printStackTrace();
+    }
+  }
+}
